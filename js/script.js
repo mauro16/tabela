af5102e2a9f4f7f84b86759a66aebbb576f54e90
mauro16 <mauro.silva.skype@gmail.com>
2019-08-14 01:47:25 +0200

    across

--- js/script.js
+++ js/script.js
@@ -1,3 +1,76 @@
+var selectedRow = null;
+
+function onFormSubmit() {
+    var formData = readFormData();
+    if (selectedRow == null)
+        insertNewRecord(formData)
+    else
+        updateRecord(formData);
+    resetForm();
+}
+
+function readFormData() {
+    var formData = {};
+    formData["name"] = document.getElementById("name").value;
+    formData["m"] = document.getElementById("m").value;
+    formData["s"] = document.getElementById("s").value;
+    formData["g"] = document.getElementById("g").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("table").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.name;
+
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.m;
+
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.s;
+
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.g;
+
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<button type="button" onClick="onEdit(this)" class="btn btn-primary">Edit</button>
+                       <button type="button" onClick="onDelete(this)" class="btn btn-danger">Delete</button> `;
+}
+
+function resetForm() {
+    document.getElementById("name").value = "";
+    document.getElementById("m").value = "";
+    document.getElementById("s").value = "";
+    document.getElementById("g").value = "";
+    document.getElementById("name").focus();
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("name").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("m").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("s").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("g").value = selectedRow.cells[3].innerHTML;
+}
+
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.name;
+    selectedRow.cells[1].innerHTML = formData.m;
+    selectedRow.cells[2].innerHTML = formData.s;
+    selectedRow.cells[3].innerHTML = formData.g;
+}
+
+function onDelete(td) {
+    if (confirm('deseja deletar isso?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("table").deleteRow(row.rowIndex);
+        resetForm();
+    }
+
+}
 
 txtM = document.getElementById("m").value,
     txtS = document.getElementById("s").value,
@@ -6,12 +79,33 @@ txtM = document.getElementById("m").value,
 //GET DATE AND TIME
 var dateAtualEL = document.querySelector('#dataAtual');
 var horaAtualEL = document.querySelector('#horaAtual');
+var dia = document.querySelector('#msg');
 setInterval(() => {
     var dt = new Date().toLocaleDateString('DE-de', { day: "2-digit", month: "long", year: "numeric" });
     var hr = new Date().toLocaleTimeString('DE-de');
     dateAtualEL.innerHTML = dt;
     horaAtualEL.innerHTML = hr;
+
+    if (hr < 5) {
+        dia.innerHTML = "Guten Abend";
+    }
+    else
+        if (hr < 8) {
+            dia.innerHTML = "guten morgen!";
+        }
+        else
+            if (hr < 12) {
+                dia.innerHTML = "guten morgen!";
+            }
+            else
+                if (hr < 18) {
+                    dia.innerHTML = "Guten nachmittag!";
+                }
+                else {
+                    dia.innerHTML = "Guten Abend!";
+                }
 }, 1000);
+
 //*END* GET DATE AND TIME
 
 $(document).ready(function () {
@@ -27,111 +121,6 @@ $(document).on("click", ".cel1", ".cel2", function () {
     $('#g').val(nomePessoa);
 });
 
-
-//INPUT TEXT VALIDATION
-var rIndex, table = document.getElementById("table")
-function checkEmptyInput() {
-
-    var isEmpty = false;
-    txtName = document.getElementById("name").value;
-    txtM = document.getElementById("m").value;
-    txtS = document.getElementById("s").value;
-    txtG = document.getElementById("g").value;
-
-    if (txtName === "") {
-        alert("Name is empty!")
-        isEmpty = true;
-    }
-    else if (txtM === "") {
-        alert("Mundlich is empty!")
-        isEmpty = true;
-    }
-    else if (txtS === "") {
-        alert("Schriblich is empty!")
-        isEmpty = true;
-    }
-    else if (txtG === "") {
-        alert("Gesamt is empty!")
-        isEmpty = true;
-    }
-    return isEmpty;
-}
-//END INPUT TEXT VALIDATION
-
-//ADD ROWS
-function addHTMLTableRow() {
-    //Pega a table apelo ID
-    //cria uma nova linha e uma nova celula
-    //pega o valor dos inputs
-    //coloca os valores dentros da linhas
-    if (!checkEmptyInput()) {
-        var newRow = table.insertRow(table.length),
-            cell1 = newRow.insertCell(0),
-            cell2 = newRow.insertCell(1),
-            cell3 = newRow.insertCell(2),
-            cell4 = newRow.insertCell(3),
-
-            txtName = document.getElementById("name").value,
-            txtM = document.getElementById("m").value,
-            txtS = document.getElementById("s").value,
-            txtG = document.getElementById("g").value;
-
-        cell1.innerHTML = txtName;
-        cell2.innerHTML = txtM;
-        cell3.innerHTML = txtS;
-        cell4.innerHTML = txtG;
-        //chama a funcao para setar o evento para a nova linha
-        selectRowInput()
-    }
-}
-//END ADD ROWS
-
-//SELECT ROW INPUT
-function selectRowInput() {
-
-
-    for (var i = 1; i < table.rows.length; i++) {
-        table.rows[i].onclick = function () {
-            //pegar a linha selecionada
-            rIndex = this.rowIndex;
-            document.getElementById("name").value = this.cells[0].innerHTML;
-            document.getElementById("m").value = this.cells[1].innerHTML;
-            document.getElementById("s").value = this.cells[2].innerHTML;
-            document.getElementById("g").value = this.cells[3].innerHTML;
-        };
-    }
-}
-selectRowInput()
-//END SELECT ROW INPUT
-
-//EDIT TABLE SELECTED ROW
-function editHtmlTableSelectedRow() {
-    var txtName = document.getElementById("name").value,
-        txtM = document.getElementById("m").value,
-        txtS = document.getElementById("s").value,
-        txtG = document.getElementById("g").value;
-
-    if (!checkEmptyInput()) {
-        table.rows[rIndex].cells[0].innerHTML = txtName;
-        table.rows[rIndex].cells[1].innerHTML = txtM;
-        table.rows[rIndex].cells[2].innerHTML = txtS;
-        table.rows[rIndex].cells[3].innerHTML = txtG;
-    }
-
-}
-//END EDIT TABLE SELECTED ROW
-
-//DELETE 
-function deleta() {
-    table.deleteRow(rIndex)
-
-    //Limpa os campos
-    document.getElementById("name").value = "";
-    document.getElementById("m").value = "";
-    document.getElementById("s").value = "";
-    document.getElementById("g").value = "";
-}
-// END DELETE
 
 //Export Table to Excel
 function exportTableToExcel(tableID, filename = '') {
@@ -245,13 +234,21 @@ function HorizonzalS() {
     });
 }
 //END HORIZONTAL INPUT TEXT "TxtSchriftlich"
+// Function to observe the inputs and take the intersection
 
-//TABLE ARRAY
-var tableInfo = Array.prototype.map.call(document.querySelectorAll('.searchtbl tr'), function (tr) {
-    return Array.prototype.map.call(tr.querySelectorAll('td'), function (td) {
-        return td.innerText;
+$('#m, #s').on('blur', (e) => {
+    linhas = document.querySelectorAll('.searchtbl tr');
+    across = linhas[Vindex].cells[Hindex]
+    $('#g').val($(across).text());
+    $(across).addClass("highlighted");
+})
+
+//Botao Pesquisar
+$(document).ready(function () {
+    $("#txtBuscar").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#table tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
     });
-
 });
-console.log(tableInfo)
-//END TABLE ARRAY
